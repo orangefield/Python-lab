6cb759d2a86ccd90c7678ffd57ab2a6abcf9a85f
field <jsw_777@naver.com>
2022-04-26 12:47:55 +0900

    1. 파이썬 자료형 정수, 문자

---
+++ ex01/var1.py
@@ -0,0 +1,39 @@
+# python 자료형
+
+# 정수
+n1 = 1
+
+# 실수
+n2 = 1.1
+
+# 문자열
+s1 = '문자'
+
+# 긴 문자열
+s2 = '''
+안녕하세요
+반갑습니다
+ㅋㅋㅋㅋㅋ
+'''
+
+# 문자열 안에 쌍따옴표 넣기
+s3 = '그는 말했다. "반가워" 라고'
+
+# 문자 안에 변수 바인딩 f-string
+username = '홍길동'
+s4 = f'안녕 내이름은 {username}이야'
+
+print(n1) 
+print(n2) 
+print(s1) 
+print(s2) 
+print(s3) 
+print(s4)
+
+print('='*50)
+
+print(type(n1)) 
+print(type(n2)) 
+print(type(s1)) 
+print(type(s2)) 
+print(type(s3)) 
